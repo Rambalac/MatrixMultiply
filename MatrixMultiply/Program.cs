03e6326fe0b74a2f998e43b129d9f7fb4a025571
Rambalac <rambalac+github@gmail.com>
2016-09-02 23:17:24 +0900

    Add project files.

---
+++ MatrixMultiply/Program.cs
@@ -0,0 +1,165 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Diagnostics.Contracts;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MatrixMultiply
+{
+    public class Matrix
+    {
+        public int Width { get; }
+        public int Height { get; }
+
+        private float[][] m;
+
+
+        public Matrix(int width, int height)
+        {
+            Width = width;
+            Height = height;
+            m = new float[height][];
+            for (int row = 0; row < height; row++)
+            {
+                m[row] = new float[width];
+            }
+        }
+
+        public Matrix Transpose()
+        {
+            var result = new Matrix(Height, Width);
+            for (int row = 0; row < Height; row++)
+            {
+                for (int col = 0; col < Width; col++)
+                {
+                    result.m[col][row] = m[row][col];
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix Multiply(Matrix a, Matrix b)
+        {
+            Contract.Requires(a.Width == b.Height && a.Height == b.Width);
+
+            var result = new Matrix(b.Width, a.Height);
+
+            var tempb = b.Transpose();
+
+            for (int row = 0; row < result.Height; row++)
+            {
+                Console.WriteLine(row);
+                var resultrow = result.m[row];
+                for (int col = 0; col < result.Width; col++)
+                {
+                    float sum = 0;
+                    var arow = a.m[row];
+                    var bcol = tempb.m[col];
+                    for (int j = 0; j < a.Width; j++)
+                    {
+                        sum += arow[j] * bcol[j];
+                    }
+                    resultrow[col] = sum;
+                }
+            }
+
+            return result;
+        }
+
+        public static Matrix ParallelMultiply(Matrix a, Matrix b)
+        {
+            Contract.Requires(a.Width == b.Height && a.Height == b.Width);
+
+            var result = new Matrix(b.Width, a.Height);
+
+            var tempb = b.Transpose();
+
+            Parallel.For(0, result.Height, (row) =>
+            {
+                //Console.WriteLine(row);
+                var resultrow = result.m[row];
+                for (int col = 0; col < result.Width; col++)
+                {
+                    float sum = 0;
+                    var arow = a.m[row];
+                    var bcol = tempb.m[col];
+                    for (int j = 0; j < a.Width; j++)
+                    {
+                        sum += arow[j] * bcol[j];
+                    }
+                    resultrow[col] = sum;
+                }
+            });
+
+            return result;
+        }
+
+        public void SetAt(int row, int col, float value)
+        {
+            m[row][col] = value;
+        }
+
+        public float GetAt(int row, int col)
+        {
+            return m[row][col];
+        }
+
+    }
+
+    class Program
+    {
+        static void AssignRandom(Matrix matrix)
+        {
+            Random rnd = new Random();
+            for (int row = 0; row < matrix.Height; row++)
+            {
+                for (int col = 0; col < matrix.Width; col++)
+                {
+                    matrix.SetAt(row, col, (float)rnd.NextDouble());
+                }
+            }
+
+        }
+
+        static void Test()
+        {
+            Matrix a = new Matrix(4096, 4096);
+            Matrix b = new Matrix(4096, 4096);
+
+            AssignRandom(a);
+            AssignRandom(b);
+
+            Matrix result = Matrix.Multiply(a, b);
+        }
+
+        static void ParallelTest()
+        {
+            Matrix a = new Matrix(4096, 4096);
+            Matrix b = new Matrix(4096, 4096);
+
+            AssignRandom(a);
+            AssignRandom(b);
+
+            Matrix result = Matrix.ParallelMultiply(a, b);
+        }
+
+        static void Main(string[] args)
+        {
+            Stopwatch watch = Stopwatch.StartNew();
+
+            Test();
+
+            Console.WriteLine("Single threaded Test elapsed seconds: " + watch.Elapsed.TotalSeconds);
+
+            watch = Stopwatch.StartNew();
+
+            ParallelTest();
+
+            Console.WriteLine("Parallel Test elapsed seconds: " + watch.Elapsed.TotalSeconds);
+
+        }
+    }
+}
